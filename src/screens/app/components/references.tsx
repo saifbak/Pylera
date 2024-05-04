20e9e0f6ce7f81330e7c5a615db96c2469bd6d90
saifbak <saif@enorness.com>
2024-05-04 18:40:32 +0400

    disclaimers and reference screen added

--- src/screens/app/components/references.tsx
+++ src/screens/app/components/references.tsx
@@ -1,23 +1,29 @@
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
+import { Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React, { FC } from 'react'
-import { ScaledSheet } from 'react-native-size-matters'
-import { COLORS, ICONS } from '../../../shared/utils/theme'
+import { ScaledSheet, vs } from 'react-native-size-matters'
+import { COLORS, FONTS, ICONS } from '../../../shared/utils/theme'
+import i18next, { t } from 'i18next'
+import { useTranslation } from 'react-i18next'
 
 type referenceProps = {
     navigation: any
 }
 
 const References: FC<referenceProps> = ({ navigation }) => {
+    const { t } = useTranslation()
     const handleGoBack = () => {
         navigation.goBack();
     }
     return (
         <View style={styles.container}>
-            <View style={styles.content}>
+            <View style={[styles.content, { alignItems: i18next.language === "ar" ? "flex-end" : "flex-start" }]}>
                 <Pressable onPress={handleGoBack}>
                     <Image source={ICONS.cross} style={styles.iconStyle} />
                 </Pressable>
-                <Text>references</Text>
+                <Text style={{ alignSelf: i18next.language === "ar" ? "flex-end" : "flex-start", fontFamily: i18next.language === "ar" ? FONTS.text_arabic : FONTS.semibold, color: "#af0f10", fontSize: vs(14), marginTop: vs(20) }}>{t('references')}</Text>
+                <ScrollView style={{ flex: 1, }}>
+                    <Text style={{ alignSelf: i18next.language === "ar" ? "flex-end" : "flex-start", fontFamily: i18next.language === "ar" ? FONTS.text_arabic : FONTS.semibold, color: "#af0f10", fontSize: vs(14), marginTop: vs(20) }}>{t('references')}</Text>
+                </ScrollView>
             </View>
         </View>
     )
